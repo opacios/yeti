39dd45e09cd2ad4f05184ea161d01e70f4a127e0
Marci Fosso <marcifosso@yahoo.co.uk>
2018-06-15 16:50:33 +0100

    create settings file

---
+++ web/sites/default/settings.php
@@ -0,0 +1,13 @@
+<?php
+$settings['hash_salt'] = 'ZX_VWgsoor3Gl3cy_GAwtzHt31Mx3d-vxRJ_B5Yk_A5RIioosf_u2OQhYP6AZin-aHr1gOg5mg';
+$config_directories['sync'] = '../config/sync';
+$databases['default']['default'] = array (
+  'database' => 'web18db',
+  'username' => 'drupal',
+  'password' => 'Drup@l',
+  'prefix' => 'web18',
+  'host' => 'postgresql',
+  'port' => '5432',
+  'driver' => 'pgsql',
+);
+$settings['install_profile'] = 'standard';
